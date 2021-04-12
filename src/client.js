4f73f0c80e3c7024babd193e077909522b151833
Rishabh Mehta <44340780+Rish7474@users.noreply.github.com>
2021-04-12 16:30:38 -0500

    Update client.js

--- src/client.js
+++ src/client.js
@@ -11,33 +11,33 @@ app.get('/', function(request, response) {
     response.send(result);
 }).listen(app.get('port'), function() {
     console.log('App is running, server is listening on port ', app.get('port'));
-});
+    const {Client} = require('discord.js');
+    const client = new Client();
 
-const {Client} = require('discord.js');
-const client = new Client();
+    const { Player } = require('discord-player');
+    client.player = new Player(client);
 
-const { Player } = require('discord-player');
-client.player = new Player(client);
+    const ActionHandler = require('./actions.js');
+    actions = new ActionHandler(client.player);
 
-const ActionHandler = require('./actions.js');
-actions = new ActionHandler(client.player);
+    client.on('ready', () => {
+        console.log(`${client.user.tag} is online`);
+    });
 
-client.on('ready', () => {
-    console.log(`${client.user.tag} is online`);
-});
-
-client.on('message', async (eventInfo) => {
-    if (eventInfo.content[0] === process.env.INVOKE_TAG) {
-        let cmdQuery = eventInfo.content.substring(1);
-        status = await actions.processAction(eventInfo, cmdQuery);
-        eventInfo.channel.send(status[1]);
-    }
-    else {
-        let cmdQuery = eventInfo.content;
-        status = await actions.processNaturalLanguage(eventInfo, cmdQuery, process.env.BOT_NAME);
-        if(status[0] != undefined)
+    client.on('message', async (eventInfo) => {
+        if (eventInfo.content[0] === process.env.INVOKE_TAG) {
+            let cmdQuery = eventInfo.content.substring(1);
+            status = await actions.processAction(eventInfo, cmdQuery);
             eventInfo.channel.send(status[1]);
-    }
+        }
+        else {
+            let cmdQuery = eventInfo.content;
+            status = await actions.processNaturalLanguage(eventInfo, cmdQuery, process.env.BOT_NAME);
+            if(status[0] != undefined)
+                eventInfo.channel.send(status[1]);
+        }
+    });
+
+    client.login(process.env.DISCORDJS_BOT_TOKEN);
 });
 
-client.login(process.env.DISCORDJS_BOT_TOKEN);
