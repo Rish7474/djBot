590cf21dd1b2aa0205e427b0ef19ef3cfab6906e
rish <rishmehta2001@icloud.com>
2021-04-12 20:07:00 -0500

    express hosting set up

--- src/client.js
+++ src/client.js
@@ -1,18 +1,5 @@
 require('dotenv').config();
 
-var express = require('express');
-var app     = express();
-
-app.set('port', (process.env.PORT || 5000));
-
-//For avoidong Heroku $PORT error (found on https://bit.ly/2RjMRGj)
-app.get('/', function(request, response) {
-    var result = 'App is running'
-    response.send(result);
-}).listen(app.get('port'), function() {
-    console.log('App is running, server is listening on port ', app.get('port'));
-});
-
 const {Client} = require('discord.js');
 const client = new Client();
 
